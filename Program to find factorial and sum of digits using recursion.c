d3e80be8ed7a83b96bd987151acbeac99a037837
appu14-hub14.he/him <appu.madar@s.amity.edu>
2026-03-06 10:14:20 +0530

    first save

---
+++ Program to find factorial and sum of digits using recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num, fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    fact = factorial(num);
+
+    printf("Factorial of %d = %d", num, fact);
+
+    return 0;
+}
